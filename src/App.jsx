f967b4a710c165162144202ba071f267b57db3d2
AngelMonoestereo <angel.thevinylpourclub.com>
2025-07-31 18:26:46 -0400

    Sidebar toggle funcionando correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import Video from './Pages/Video/Video';
+
+const App = () => {
+  const [sidebar, setSidebar] = useState(true); // 👈 Manejamos el estado aquí
+
+  return (
+    <div>
+      <Navbar toggleSidebar={() => setSidebar(!sidebar)} /> {/* 👈 Pasamos la función */}
+      <Routes>
+        <Route path="/" element={<Home sidebar={sidebar} />} />
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
+
+
+
 // import React, { useState } from 'react';
 // import { Routes, Route } from 'react-router-dom';
 // import Navbar from './Components/Navbar/Navbar';
@@ -23,24 +47,24 @@
 
 // export default App
 
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-import Home from './Pages/Home/Home';
-import Video from './Pages/Video/Video'; // make sure this file exists
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import Navbar from './Components/Navbar/Navbar';
+// import Home from './Pages/Home/Home';
+// import Video from './Pages/Video/Video'; // make sure this file exists
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home sidebar={true} />} />
-        <Route path="/video/:categoryId/:videoId" element={<Video />} />
-      </Routes>
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home sidebar={true} />} />
+//         <Route path="/video/:categoryId/:videoId" element={<Video />} />
+//       </Routes>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
 
 
